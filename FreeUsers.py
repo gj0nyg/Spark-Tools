d0163e41b4f1e3bfa6cd2609445aeddbbde68325
Joe Bennett <joe.bennett@team.telstra.com>
2017-09-15 22:17:53 +1000

    Added FreeUsers
    Iterates through the userlist and prints all with no licence assigned

--- FreeUsers.py
+++ FreeUsers.py
@@ -1,25 +1,17 @@
 #!/usr/local/bin/python3
+# Code borrows heavily from Cisco Devnet lessons
 from ciscosparkapi import CiscoSparkAPI, SparkApiError
 import os
 import sys
-# Licence UUIDs can be obtained from https://developer.ciscospark.com/endpoint-licenses-get.html
-A25UserMeeting="Replace with UUID for meeting"
-WebExMeeting="Replace with WebEx UUID"
-Messaging="Replace with Messaging licence UUID"
-WebExCMR="Replace with CMR UUID"
-licences=[A25UserMeeting,WebExMeeting,WebExCMR,Messaging]
 
 if __name__ == '__main__':
 
         # Command line arguments parsing    
         from argparse import ArgumentParser  
         parser = ArgumentParser("ConvertUsers.py")  
-        parser.add_argument("-e", "--email", help="the email address of the user that you wish to convert", required=True)
         parser.add_argument("-t", "--token", help="[optional] your admin access token. Alternatively, you can use the SPARK_ACCESS_TOKEN env variable", required=False)
         args = parser.parse_args() 
         access_token = args.token
-        email = args.email
-
         # Check access token
         spark_access_token = os.environ.get("SPARK_ACCESS_TOKEN")
         token = access_token if access_token else spark_access_token
@@ -31,18 +23,15 @@ if __name__ == '__main__':
             print(error_message)
             sys.exit(2)
         try:
+            FreeUser=[]
             api = CiscoSparkAPI(access_token=token)
-            users=api.people.list(email=email)
+            users=api.people.list()
             for user in users:  
                 userDetails=api.people.get(personId=user.id)
-                api.people.update(
-                    personId=user.id,emails=userDetails.emails,
-                    displayName=userDetails.displayName,firstName=userDetails.firstName,
-                    lastName=userDetails.lastName,avatar=userDetails.avatar,
-                    orgId=userDetails.orgId,roles=userDetails.roles,licenses=licences
-                    )
-            
-            print("User %s was updated" %userDetails.displayName)
+                if userDetails.licenses == []:
+                    FreeUser.append(userDetails.emails[0])
+                    print (userDetails.emails[0])            
+#            print(FreeUser)
         except SparkApiError as e:
             print("failed with statusCode: %d" % e.response_code)
             if e.response_code == 404:
